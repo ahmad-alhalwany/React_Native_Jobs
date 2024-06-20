317956b3ae6741dcf078b9bd3d539e9afe7e47f7
ahmad-alhalwany <ahmad.alhalwany@lit-co.net>
2024-06-20 19:01:11 +0300

    [#3]: implement Popularjobs + Nearbyjobs for project

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,16 +1,26 @@
-import {useState} from 'react'
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
-import { useRouter } from 'expo-router'
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from "react-native";
 
-import styles from './popularjobs.style'
-import { COLORS, SIZES } from '../../../constants'
-import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import styles from "./popularjobs.style";
 
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
     <View style={styles.container}>
@@ -28,10 +38,12 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4,]}
+            data={data}
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={(item) => item.job_id}
@@ -41,7 +53,7 @@ const Popularjobs = () => {
         )}
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
